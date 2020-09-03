de662600d22b2737095aa0237a28c1d83a0ad9ec
Jaskaran Bir Singh Jaskaran Bir Singh <jjaskara@isimtl.local>
2020-09-03 16:45:51 -0400

    css changes

--- src/component/home.js
+++ src/component/home.js
@@ -2,6 +2,7 @@ import React, { Component  } from 'react';
 import {Redirect} from 'react-router-dom';
 import '../home.css';
 import history from './../history';
+import Header from './common/header'
 export default class Home extends Component
 {
     constructor(props) {
@@ -76,27 +77,7 @@ render()
     }
   return (
     <div class="bk-white">
-	  <nav class="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-      <div class="container">
-        <a class="navbar-brand" href="home.html">Montreal<i> Times</i></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="oi oi-menu"></span> Menu
-        </button>
-
-        <div class="collapse navbar-collapse" id="ftco-nav">
-          <ul class="navbar-nav ml-auto">
-          <li class="nav-item active"><a onClick={()=> history.push('/favourite')} class="nav-link">Favourites</a></li>
-            {this.state.category.map((opt) =>(<li class="nav-item active"><a href="#" class="nav-link">{opt.name}</a></li>))}
-            {/* <li class="nav-item active"><a href="#" class="nav-link">NEWS</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">TRAVEL</a></li>           
-            <li class="nav-item"><a href="#" class="nav-link">CONTACT</a></li> */}
-            <li class="nav-item"><a onClick={()=> history.push('/profile')} class="nav-link">Profile</a></li>
-            <li class="nav-item"><a onClick={()=>{this.signout()}} class="nav-link">Signout</a></li>
-              
-          </ul>
-        </div>
-      </div>
-    </nav>
+        <Header/>
         <div class="hero-wrap js-fullheight" style={styleRed} data-stellar-background-ratio="0.5">
         {/* <div class="hero-wrap js-fullheight"  data-stellar-background-ratio="0.5"> */}
             <div class="overlay"></div>
