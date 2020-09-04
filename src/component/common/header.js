f4f2064e0e5057de5725a3a96ceaa28388c023b6
jaskaran <developer.jaskaran@gmail.com>
2020-09-04 14:16:00 -0400

    home category data

--- src/component/common/header.js
+++ src/component/common/header.js
@@ -61,7 +61,6 @@ export default class Home extends Component
     }
     render() 
     {
-<<<<<<< HEAD
       return (
         <nav class="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
@@ -82,39 +81,5 @@ export default class Home extends Component
     </div>
   </nav>
       )
-=======
-      if(this.state.login)
-      {
-        return (
-          <nav class="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-          <div class="container">
-            <a class="navbar-brand" href="/">Montreal<i> Times</i></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="oi oi-menu"></span> Menu
-            </button>
-
-            <div class="collapse navbar-collapse" id="ftco-nav">
-              <ul class="navbar-nav ml-auto"> 
-                <li class="nav-item active"><a onClick={()=> history.push('/favourite')} class="nav-link">Favourites</a></li>
-                {this.state.category.map((opt) =>(<li class="nav-item active"><a href="#" class="nav-link">{opt.name}</a></li>))}
-                <li class="nav-item"><a onClick={()=> history.push('/profile')} class="nav-link">Profile</a></li>
-                <li class="nav-item"><a onClick={()=>{this.signout()}} class="nav-link">Signout</a></li> 
-                  
-              </ul>
-            </div>
-          </div>
-          </nav>
-        )
-      }
-      else{
-        return (
-          <nav class="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-          <div class="container">
-            <a class="navbar-brand" href="/">Montreal<i> Times</i></a>
-          </div>
-          </nav>
-        )
-      }
->>>>>>> 324b70f20113dbd72e6ec6e780364a25bb14e5cb
     }
 }
